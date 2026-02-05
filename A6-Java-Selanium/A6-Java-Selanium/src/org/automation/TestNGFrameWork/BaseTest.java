314d41fee8b8091d4fc63dee9e00a28b0c9c8361
HP <HP@DESKTOP-KO8IGEB>
2026-02-05 11:59:06 +0530

    Updated code2

--- A6-Java-Selanium/A6-Java-Selanium/src/org/automation/TestNGFrameWork/BaseTest.java
+++ A6-Java-Selanium/A6-Java-Selanium/src/org/automation/TestNGFrameWork/BaseTest.java
@@ -1,15 +1,11 @@
 package org.automation.TestNGFrameWork;
 
-import java.time.Duration;
-
 import org.automation.genericUtilities.Flib;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
